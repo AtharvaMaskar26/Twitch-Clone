4c4b68fa7a75719668ea0ae16e8541cb9204136e
atharva26aug <atharvamaskar10@gmail.com>
2024-01-17 12:33:42 +0530

    Implemented Video Component

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -2,6 +2,10 @@
 
 import { useViewerToken } from '@/hooks/use-viewer-token';
 import { Stream, User } from '@prisma/client'
+import {LiveKitRoom} from '@livekit/components-react' 
+
+import { Video } from './video';
+
 import React from 'react'
 
 interface StreamPlayerProps {
@@ -33,7 +37,20 @@ const StreamPlayer = ({
 
 
   return (
-    <div>Allowed to Watch The Stream</div>
+    <>
+      <LiveKitRoom
+        token={token}
+        serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+        className='grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full'
+      >
+        <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+          <Video
+            hostName={user.username}
+            hostIdentity={user.id}
+           />
+        </div>
+      </LiveKitRoom>
+    </>
   )
 }
 
